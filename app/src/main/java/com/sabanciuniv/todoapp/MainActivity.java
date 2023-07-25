27ee6985fdd691f00a4686d7b5640b66651c4b16
Bilal Dertli <127341202+bilalbdertli@users.noreply.github.com>
2023-07-25 19:27:21 +0300

    added recView and set the main activity's design

--- app/src/main/java/com/sabanciuniv/todoapp/MainActivity.java
+++ app/src/main/java/com/sabanciuniv/todoapp/MainActivity.java
@@ -3,19 +3,36 @@ package com.sabanciuniv.todoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sabanciuniv.todoapp.model.ToDo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recView;
+    List<ToDo> todos =new ArrayList<>();
+    TextView zeroTodos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recView =findViewById(R.id.recTodo);
+        zeroTodos = findViewById(R.id.zeroTodo);
+        getSupportActionBar().setTitle("To-Do's of the Day");
+
+
     }
 
     @Override
@@ -37,5 +54,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recView.setVisibility(View.VISIBLE);
+        zeroTodos.setVisibility(View.INVISIBLE);
+
     }
 }
